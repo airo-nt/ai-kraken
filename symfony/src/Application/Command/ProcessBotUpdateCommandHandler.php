c9c344a2a6a888364db039eb6254806859b52657
Airat Agliullin <ai.agliullin@profit.tech>
2026-04-29 16:44:56 +0400

    Add handle exceptions by validation and ai communication

--- symfony/src/Application/Command/ProcessBotUpdateCommandHandler.php
+++ symfony/src/Application/Command/ProcessBotUpdateCommandHandler.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace App\Application\Command;
 
 use App\Application\Client\Ai\AiClientInterface;
+use App\Application\Client\Ai\Exception\AiClientException;
 use App\Application\Client\Telegram\TelegramClientInterface;
 use App\Domain\Model\Chat;
 use App\Domain\Model\ValueObject\ChatId;
 use App\Domain\Repository\ChatRepositoryInterface;
+use App\Domain\Repository\Exception\ValidationException;
 
 final class ProcessBotUpdateCommandHandler
 {
@@ -21,18 +23,30 @@ final class ProcessBotUpdateCommandHandler
     public function __invoke(ProcessBotUpdateCommand $command): void
     {
         $chatId = new ChatId($command->getChatId());
+        $chatIdValue = $chatId->getValue();
         $isExistsChat = $this->chatRepository->isExistsByChatId($chatId);
         if (!$isExistsChat) {
             $chat = new Chat($chatId, $command->getUserName());
-            $this->chatRepository->create($chat);
+            try {
+                $this->chatRepository->create($chat);
+            } catch (ValidationException) {
+                $this->telegramClient->sendMessage($chatIdValue, 'Something went wrong. Please try later.');
+                return;
+            }
         }
 
         if ($command->getText() === '/start') {
-            $this->telegramClient->sendMessage($command->getChatId(), "Hello, I'm AiKraken. Ask your questions.");
+            $this->telegramClient->sendMessage($chatIdValue, "Hello, I'm AiKraken. Ask your questions.");
             return;
         }
 
-        $response = $this->aiClient->chatCompletion($command->getText());
-        $this->telegramClient->sendMessage($command->getChatId(), $response->getAiMessage());
+        $this->telegramClient->sendMessage($chatIdValue, 'Please wait. Generating answer...');
+        try {
+            $response = $this->aiClient->chatCompletion($command->getText());
+        } catch (AiClientException) {
+            $this->telegramClient->sendMessage($chatIdValue, 'Something went wrong. Please try later.');
+            return;
+        }
+        $this->telegramClient->sendMessage($chatIdValue, $response->getAiMessage());
     }
 }
